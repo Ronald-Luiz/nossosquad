90e2d4623da2d4817505b1653cf8840caf79dada
Ronald-Luiz <56053290+Ronald-Luiz@users.noreply.github.com>
2022-10-26 12:16:00 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+function Login() {
+  var done = 0;
+  var usuario = document.getElementsByName('usuario')[0].value;
+  usuario = usuario.toLowerCase();
+  var senha = document.getElementsByName('senha')[0].value;
+  seha = senha.toLowerCase();
+  if (usuario == "admin" && senha == "admin") {
+
+    
+    //Swal.fire({  
+      //icon: 'success',
+      //title: 'Logado com sucesso!',
+      //showConfirmButton: false,
+      //timer: 100
+   // });
+
+    window.location = "index.html";
+    done = 1;
+    swal("Logado com sucesso!", "", "success");
+
+  }
+  if (done == 0) {
+    swal("Usuário/senha incorreto!", "", "error");
+  }
+}
