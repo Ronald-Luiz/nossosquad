edd04cc8c881de66e1540bc9a7297f02656345b5
pkdev95 <113390894+pkdev95@users.noreply.github.com>
2022-10-27 19:45:46 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -25,4 +25,42 @@ function Login() {
 }
 
 
+let rec = document.getElementById('px');
+rec.addEventListener('click',(event) =>{
+  event.preventDefault();
+  let ml = document.getElementById('xp').value;
+  if(ml == "admin@gmail.com" ){
+     swal("um link de recuperação de senha foi enviado ao seu email");
+  }else{
+    swal('insira um e-mail válido');
+    
+  }
+})
+
+
+let pg = document.getElementById('pk');
+if(pg.clicked){
+    document.getElementById('lkj').style.display = "block";
+}
+  
+pg.addEventListener('click', () =>{
+  document.getElementById('lkj').style.display = "block";
+
+})
+
+let telk = document.getElementById('kl');
+telk.addEventListener('click', (event) => {
+event.preventDefault();
+let vtel = document.getElementById('telxd').value;
+if(vtel == 2199997777){
+  swal("um sms foi enviado com as devidas instruções");
+}else{
+  swal("insira no formato solicitado");
+}
+
+
+}) 
+
+
+
 
