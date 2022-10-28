dad8c40d57b09a3937aaa392ac1e69a4bf5116f0
Ronald <56053290+Ronald-Luiz@users.noreply.github.com>
2022-10-28 09:01:53 -0300

    Update lista.js

--- js/lista.js
+++ js/lista.js
@@ -1,38 +1,6 @@
 
-
 var btnenv = document.getElementById('crc');
 btnenv.addEventListener('click', () =>{
- 
-  function lista() {
-    
-    let nome = document.getElementsByName('login')[0].value;
-    let email = document.getElementsByName('senha')[0].value;
-    
-    if (nome ==  "") {
-      swal({
-    title: "Error",
-    text: "Login Efetuado, Você será direcionado a página inicial!",
-    icon: "success",
-    button: false,
-    });
-      
-     }  
-    else if { 
-      swal("Error!", 
-      "Usuário ou Senha Invalidos", 
-      "Error"); }
-    }
 
-
-
-
-
-
-
-
-
-//var btnenv = document.getElementById('crc');
-//btnenv.addEventListener('click', () =>{
-
-    //swal("obrigado pelo contato, em breve entraremos em contato");
-//})
+    swal("obrigado pelo contato, em breve entraremos em contato");
+})
