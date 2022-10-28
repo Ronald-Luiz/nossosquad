822f7e4443d187544016728f4d8b276181ecda71
pkdev95 <113390894+pkdev95@users.noreply.github.com>
2022-10-27 23:43:25 -0300

    Add files via upload

--- js/lista.js
+++ js/lista.js
@@ -1,4 +1,6 @@
+
+
 var btnenv = document.getElementById('crc');
 btnenv.addEventListener('click', () =>{
-    window.location.href = "http://127.0.0.1:5500/nossosquad-main/obrigado.html";
+    swal("obrigado pelo contato, em breve entraremos em contato");
 })
