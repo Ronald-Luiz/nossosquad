6a4aa9df483393c7d2e6777979168e2928380bf2
Ronald <56053290+Ronald-Luiz@users.noreply.github.com>
2022-10-28 10:15:55 -0300

    Update lista.js

--- js/lista.js
+++ js/lista.js
@@ -1,6 +1,47 @@
+Function validar{
 
 var btnenv = document.getElementById('crc');
 btnenv.addEventListener('click', () =>{
 
-    swal("obrigado pelo contato, em breve entraremos em contato");
-})
+If(nome.value ==""){
+
+
+
+alert ("preencha todos os campos");
+
+
+}
+
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var btnenv = document.getElementById('crc');
+//btnenv.addEventListener('click', () =>{
+
+   // swal("obrigado pelo contato, em breve entraremos em contato");
+//})
