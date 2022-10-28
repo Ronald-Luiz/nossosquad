50e744c30365d603c0df674ceea8390727ace902
Ronald <56053290+Ronald-Luiz@users.noreply.github.com>
2022-10-28 08:31:22 -0300

    Update lista.js

--- js/lista.js
+++ js/lista.js
@@ -2,5 +2,11 @@
 
 var btnenv = document.getElementById('crc');
 btnenv.addEventListener('click', () =>{
+
+if(btnenv == 0){
+alert ("preencha")
+
+}eles{
+
     swal("obrigado pelo contato, em breve entraremos em contato");
 })
